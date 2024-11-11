1512e93da2971983d361d8d11fb50b26aa803f4c
AnkollaDevan <ankollade1@gmail.com>
2024-11-11 21:16:59 +0530

    added new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,22 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Components/Home'
 import './App.css'
 import Weather from './Components/Weather'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
-import './App.css'
-import umbrella_icon from './assets/umbrella.png'
+
 
 function App() {
-  const [weather, setWeather] = useState(true);
-
-  const goToWeather = () =>  {
-
-      setWeather(false);
-
-  }
-
-
   return (
-      <div id='body-main'>
-          {weather ? (
-              <div className='body'>
-                  <div className="home-div">
-                      <div className="umb-img" >
-                          <img src={umbrella_icon} />
-                      </div>
-                      <div id="heading">
-                          <h1>WEATHER NOW</h1>
-                      </div>
-                      <div>
-                          <FontAwesomeIcon icon={faCircleArrowRight} className="arrow-icon" onClick={() => goToWeather()}/>
-                      </div>
-
-                  </div>
-              </div > ) : <Weather/>}
+      <>
 
 
-      </div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/weather' element={<Weather/>}/>
+      </Routes>
 
+      </>
   )
 }
 
---
+++ src/Components/Home.jsx
@@ -0,0 +1,37 @@
+import umbrella_icon from '../assets/umbrella.png'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { Link } from 'react-router-dom'; 
+
+
+
+
+function Home() {
+    return (
+        <div className='body-main'>
+            <div className='body'>
+                <div className="home-div">
+                    <div className="umb-img" >
+                        <img src={umbrella_icon} />
+                    </div>
+                    <div id="heading">
+                        <h1>WEATHER NOW</h1>
+                    </div>
+                    <div>
+                        <Link to={'/weather'}>
+                            <FontAwesomeIcon icon={faCircleArrowRight} className="arrow-icon"/>
+                        </Link>
+                        
+                    </div>
+
+                </div>
+            </div >
+
+
+        </div>
+
+    )
+}
+
+
+export default Home;
